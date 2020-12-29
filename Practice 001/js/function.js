aa5e85b1f99dd41b3007db16ec8ab9e152976354
Shabdan <shabdan.prod@gmail.com>
2020-12-29 15:02:11 +0600

    func 2

--- Practice 001/js/function.js
+++ Practice 001/js/function.js
@@ -1,4 +1,20 @@
 "use strict";
 
+let num = 20; //global
 
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10; //local
+    console.log(num);
+}
 
+showFirstMessage("Hello World");
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
