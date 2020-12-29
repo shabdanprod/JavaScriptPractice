99c497f944922ebb31b283a7dfb746e2a347a9c1
Shabdan <shabdan.prod@gmail.com>
2020-12-29 15:14:22 +0600

    func 3

--- Practice 001/js/function.js
+++ Practice 001/js/function.js
@@ -18,3 +18,15 @@ function calc(a, b) {
 console.log(calc(4, 3));
 console.log(calc(5, 6));
 console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    
+    //
+
+
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
