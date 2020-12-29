315a052bae74e20e1ac6a2d4af890e4cb1ca7f90
Shabdan <shabdan.prod@gmail.com>
2020-12-29 13:08:44 +0600

    Ukorochil

--- Practice 001/js/script.js
+++ Practice 001/js/script.js
@@ -11,12 +11,19 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из помледних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из помледних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;  
-personalMovieDB.movies[c] = d;  
+// personalMovieDB.movies[a] = b;  
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0 < 2; i++ ) {
+    const a = prompt('Один из помледних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
